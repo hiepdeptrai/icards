0fdf396a3eed7acb8fb3002f9b0eda91e531fc18
hiepgv90 <hiep.giap@shoppie.vn>
2015-11-16 16:16:00 +0700

    Init
    Init

---
+++ app/routes.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make('merchant.index');
+});
+Route::get('khoi-tao-the', function()
+{
+	return View::make('merchant.index');
+});
+Route::get('gui-tin-nhan', function()
+{
+	return View::make('merchant.index');
+});
+Route::get('/', function()
+{
+	return View::make('merchant.index');
+});
+Route::get('/', function()
+{
+	return View::make('merchant.index');
+});
+Route::get('/', function()
+{
+	return View::make('merchant.index');
+});
+Route::get('/', function()
+{
+	return View::make('merchant.index');
+});
+Route::get('/', function()
+{
+	return View::make('merchant.index');
+});
